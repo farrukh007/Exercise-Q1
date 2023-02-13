2f9c51553b36420f5f3ea5ec1452124da72bb70a
Farrukh <farrukh007@gmail.com>
2023-02-14 00:49:03 +0500

    added Q5

---
+++ Q5/q5.js
@@ -0,0 +1,5 @@
+var famous_person;
+var message;
+famous_person = "The Messenger of Allah (peace be upon him)";
+message = "Whoever believes in one God and the hereafter, should speak what is good or remain silent.";
+console.log(famous_person + 'said, ' + '\"' + message + '\"');
