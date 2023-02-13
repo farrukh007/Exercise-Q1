536f2ea1fe75b17a718322760af12c3b21486cc5
Farrukh <farrukh007@gmail.com>
2023-02-14 01:35:27 +0500

    added q7

---
+++ Q7/q7.js
@@ -0,0 +1,14 @@
+var num1;
+var num2;
+num1 = 4;
+num2 = 4;
+console.log("Addition of ", num1, "\+", num2, "\=", num1 + num2);
+num1 = 2;
+num2 = 4;
+console.log("Multiplication of ", num1, "x", num2, "\=", num1 * num2);
+num1 = 16;
+num2 = 8;
+console.log("Substraction of ", num1, "\-", num2, "\=", num1 - num2);
+num1 = 64;
+num2 = 8;
+console.log("Division of ", num1, "\/", num2, "\=", num1 / num2);
