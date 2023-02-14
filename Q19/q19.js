15380ced1d7061e81ab812d0a41caa29d0849771
Farrukh <farrukh007@gmail.com>
2023-02-15 02:05:56 +0500

    added Q19-20

---
+++ Q19/q19.js
@@ -0,0 +1,1 @@
+console.log("Only 2 Peoples have been invited for dinner.");
---
+++ Q20/q20.ts
@@ -0,0 +1,11 @@
+var rivers : any[];
+
+rivers = ["Indus River",
+          "Amazon River",
+          "Yellow River",
+          "Mississippi River",
+          "Nile",
+          "Mekong River",
+          "Volga River",
+          "Paraguay River"
+        ];
