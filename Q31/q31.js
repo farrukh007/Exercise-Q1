9f0c999ef8f27f7be5df1afbc0ce72f3bad521ce
Farrukh <farrukh007@gmail.com>
2023-02-17 02:08:47 +0500

    Added Q31

---
+++ Q31/q31.js
@@ -0,0 +1,11 @@
+var userName2;
+userName2 = ["Secretary", "Manager", "Admin", "Assistant", "Doctor"];
+if (userName2.length > 0) {
+    console.log("We have some users.");
+}
+for (var n = userName2.length; n > 0; n--) {
+    userName2.pop();
+}
+if (userName2.length == 0) {
+    console.log("We need to find some users!");
+}
