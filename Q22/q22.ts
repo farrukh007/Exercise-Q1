3f75ad41f2258a25a9b17e66b2998281b31ad9ec
Farrukh <farrukh007@gmail.com>
2023-02-19 02:23:12 +0500

    added

---
+++ Q22/q22.ts
@@ -0,0 +1,21 @@
+let rivers2 : string[];
+
+rivers2 = ["Indus River",
+          "Amazon River",
+          "Yellow River",
+          "Mississippi River",
+          "Nile",
+          "Mekong River",
+          "Volga River",
+          "Paraguay River"
+        ];
+
+/*  for (var i=0; i<rivers2.length; i++){
+  console.log(rivers2[i+2]); //index error
+}
+*/
+
+for (var i=0; i<rivers2.length; i++){
+    console.log(rivers2[i]);
+  }
+  
